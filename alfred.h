f7fba574543eb9a150255db67d2b5189fdb8f56c
Simon Wunderlich <sw@simonwunderlich.de>
2015-11-23 20:37:25 +0100

    alfred: Start new development cycle
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- alfred.h
+++ alfred.h
@@ -20,7 +20,7 @@
  */
 
 #ifndef SOURCE_VERSION
-#define SOURCE_VERSION			"2015.2"
+#define SOURCE_VERSION			"2016.0"
 #endif
 
 #include <net/ethernet.h>
--- vis/vis.h
+++ vis/vis.h
@@ -27,7 +27,7 @@
 
 
 #ifndef SOURCE_VERSION
-#define SOURCE_VERSION				"2015.2"
+#define SOURCE_VERSION				"2016.0"
 #endif
 
 #define ALFRED_SOCK_PATH_DEFAULT		"/var/run/alfred.sock"
