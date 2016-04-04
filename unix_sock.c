open-mesh-mirror/alfred

commit 58e109973bbe8b6974a8f5d13777d949fd7054d1

alfred: Allow setting the source mac via unix sock
The server will only overwrite the mac if it is zero.
Ths allows acting in place of (non-alfred) legacy clients
by injecting their data with the correct source mac.

Signed-off-by: Dominik Heidler <dominik@heidler.eu>
[sw@simonwunderlich.de: applied some style changes]
Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

unix_sock.c
@@ -97,6 +97,7 @@ static int unix_sock_add_data(struct globals *globals,
 			      struct alfred_push_data_v0 *push,
 			      int client_sock)
 {
+	static const char zero[ETH_ALEN] = { 0 };
 	struct alfred_data *data;
 	struct dataset *dataset;
 	int len, data_len, ret = -1;
@@ -119,7 +120,13 @@ static int unix_sock_add_data(struct globals *globals,
 
 	data = push->data;
 	data_len = ntohs(data->header.length);
-	memcpy(data->source, &interface->hwaddr, sizeof(interface->hwaddr));
+
+	/* clients should set the source mac to 00:00:00:00:00:00
+	 * to make the server set the source for them
+	 */
+	if (memcmp(zero, data->source, sizeof(data->source)) == 0)
+		memcpy(data->source, &interface->hwaddr,
+		       sizeof(interface->hwaddr));
 
 	if ((int)(data_len + sizeof(*data)) > len)
 		goto err;

commit 25b4ae66dca453604e91d90d459b2523bb3da891

alfred: Only accept valid mac addresses via unix socket
Not only 00:00:00:00:00:00 but also multicast addresses are invalid as data
source for alfred. These have to be checked too before accepting the mac
address received from the client over the unix socket.

Fixes: 58e109973bbe ("alfred: Allow setting the source mac via unix sock")
Signed-off-by: Sven Eckelmann <sven@narfation.org>
Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

unix_sock.c
@@ -97,7 +97,6 @@ static int unix_sock_add_data(struct globals *globals,
 			      struct alfred_push_data_v0 *push,
 			      int client_sock)
 {
-	static const char zero[ETH_ALEN] = { 0 };
 	struct alfred_data *data;
 	struct dataset *dataset;
 	int len, data_len, ret = -1;
@@ -124,7 +123,7 @@ static int unix_sock_add_data(struct globals *globals,
 	/* clients should set the source mac to 00:00:00:00:00:00
 	 * to make the server set the source for them
 	 */
-	if (memcmp(zero, data->source, sizeof(data->source)) == 0)
+	if (!is_valid_ether_addr(data->source))
 		memcpy(data->source, &interface->hwaddr,
 		       sizeof(interface->hwaddr));
 