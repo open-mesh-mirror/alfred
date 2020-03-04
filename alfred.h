a4adede297e5d73ecc1816c115b132f67cc98016
Simon Wunderlich <sw@simonwunderlich.de>
2020-03-04 11:57:41 +0100

    alfred: Start new development cycle
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- alfred.h
+++ alfred.h
@@ -7,7 +7,7 @@
  */
 
 #ifndef SOURCE_VERSION
-#define SOURCE_VERSION			"2020.0"
+#define SOURCE_VERSION			"2020.1"
 #endif
 
 #include <net/ethernet.h>
--- vis/vis.h
+++ vis/vis.h
@@ -14,7 +14,7 @@
 
 
 #ifndef SOURCE_VERSION
-#define SOURCE_VERSION				"2020.0"
+#define SOURCE_VERSION				"2020.1"
 #endif
 
 #define ALFRED_SOCK_PATH_DEFAULT		"/var/run/alfred.sock"
