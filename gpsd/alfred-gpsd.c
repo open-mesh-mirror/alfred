open-mesh-mirror/alfred

commit 6c6a6f735054a839bd809d5711c29287505ccdd5

alfred: Make unix socket path configurable
The path to the socket used for client-server communication is currently
hardcoded in alfred and its complimentary daemons. This makes it hard to run
two instances of alfred on the same machine without any kind of virtualization
or containers.

An user may still want to use two alfred instances to create a test setup or
connect a single machine two separated alfred data clouds without exchanging
data between them.

Reported-by: Tobias Hachmer <tobias@hachmer.de>
Signed-off-by: Sven Eckelmann <sven@narfation.org>
Acked-by: Andrew Lunn <andrew@lunn.ch>
Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

gpsd/alfred-gpsd.c
@@ -36,7 +36,7 @@ static int alfred_open_sock(struct globals *globals)
 
 	memset(&addr, 0, sizeof(addr));
 	addr.sun_family = AF_LOCAL;
-	strncpy(addr.sun_path, ALFRED_SOCK_PATH, sizeof(addr.sun_path));
+	strncpy(addr.sun_path, globals->unix_path, sizeof(addr.sun_path));
 	addr.sun_path[sizeof(addr.sun_path) - 1] = '\0';
 
 	if (connect(globals->unix_sock, (struct sockaddr *)&addr,
@@ -399,6 +399,7 @@ static struct globals *gpsd_init(int argc, char *argv[])
 		{"server",	no_argument,		NULL,	's'},
 		{"location",    required_argument,	NULL,	'l'},
 		{"gpsd",	required_argument,	NULL,	'g'},
+		{"unix-path", 	required_argument,	NULL,	'u'},
 		{"help",	no_argument,		NULL,	'h'},
 		{"version",	no_argument,		NULL,	'v'},
 		{NULL,		0,			NULL,	0},
@@ -410,8 +411,9 @@ static struct globals *gpsd_init(int argc, char *argv[])
 	globals->opmode = OPMODE_CLIENT;
 	globals->source = SOURCE_GPSD;
 	globals->gpsd_format = FORMAT_JSON;
+	globals->unix_path = ALFRED_SOCK_PATH_DEFAULT;
 
-	while ((opt = getopt_long(argc, argv, "shl:g:v", long_options,
+	while ((opt = getopt_long(argc, argv, "shl:g:vu:", long_options,
 				  &opt_ind)) != -1) {
 		switch (opt) {
 		case 's':
@@ -424,6 +426,9 @@ static struct globals *gpsd_init(int argc, char *argv[])
 		case 'g':
 			gpsd_source_spec(optarg, &globals->gpsdsource);
 			have_source = true;
+			break;
+		case 'u':
+			globals->unix_path = optarg;
 			break;
 		case 'v':
 			printf("%s %s\n", argv[0], SOURCE_VERSION);

alfred.h
@@ -35,7 +35,7 @@
 #define ALFRED_REQUEST_TIMEOUT		10
 #define ALFRED_SERVER_TIMEOUT		60
 #define ALFRED_DATA_TIMEOUT		600
-#define ALFRED_SOCK_PATH		"/var/run/alfred.sock"
+#define ALFRED_SOCK_PATH_DEFAULT	"/var/run/alfred.sock"
 #define NO_FILTER			-1
 
 enum data_source {
@@ -102,6 +102,7 @@ struct globals {
 
 	int netsock;
 	int unix_sock;
+	const char *unix_path;
 
 	struct timespec if_check;
 
@@ -144,8 +145,8 @@ ssize_t send_alfred_packet(struct globals *globals, const struct in6_addr *dest,
 			   void *buf, int length);
 /* unix_sock.c */
 int unix_sock_read(struct globals *globals);
-int unix_sock_open_daemon(struct globals *globals, const char *path);
-int unix_sock_open_client(struct globals *globals, const char *path);
+int unix_sock_open_daemon(struct globals *globals);
+int unix_sock_open_client(struct globals *globals);
 int unix_sock_close(struct globals *globals);
 int unix_sock_req_data_finish(struct globals *globals,
 			      struct transaction_head *head);

client.c
@@ -40,7 +40,7 @@ int alfred_client_request_data(struct globals *globals)
 	int ret, len, data_len, i;
 	const size_t buf_data_len = sizeof(buf) - sizeof(*push) - sizeof(*data);
 
-	if (unix_sock_open_client(globals, ALFRED_SOCK_PATH))
+	if (unix_sock_open_client(globals))
 		return -1;
 
 	request = (struct alfred_request_v0 *)buf;
@@ -146,7 +146,7 @@ int alfred_client_set_data(struct globals *globals)
 	struct alfred_data *data;
 	int ret, len;
 
-	if (unix_sock_open_client(globals, ALFRED_SOCK_PATH))
+	if (unix_sock_open_client(globals))
 		return -1;
 
 	push = (struct alfred_push_data_v0 *)buf;
@@ -187,7 +187,7 @@ int alfred_client_modeswitch(struct globals *globals)
 	struct alfred_modeswitch_v0 *modeswitch;
 	int ret, len;
 
-	if (unix_sock_open_client(globals, ALFRED_SOCK_PATH))
+	if (unix_sock_open_client(globals))
 		return -1;
 
 	modeswitch = (struct alfred_modeswitch_v0 *)buf;

gpsd/alfred-gpsd.h
@@ -41,7 +41,7 @@
 #define SOURCE_VERSION				"2014.4.0"
 #endif
 
-#define ALFRED_SOCK_PATH			"/var/run/alfred.sock"
+#define ALFRED_SOCK_PATH_DEFAULT		"/var/run/alfred.sock"
 #define PATH_BUFF_LEN				200
 #define GPSD_PACKETTYPE				2
 #define	GPSD_PACKETVERSION			1
@@ -95,6 +95,7 @@ struct globals {
 
 	float lat, lon, alt;
 	int unix_sock;
+	const char *unix_path;
 
 	struct fixsource_t gpsdsource;
 	struct gps_data_t gpsdata;

gpsd/man/alfred-gpsd.8
@@ -43,6 +43,9 @@ Print the version
 \fB\-h\fP, \fB\-\-help\fP
 Display a brief help message.
 .TP
+\fB\-u\fP, \fB\-\-unix-path\fP \fIpath\fP
+path to unix socket used for alfred server communication.
+.TP
 \fB\-s\fP, \fB\-\-server\fP
 Start up in server mode. This server will read the current location
 from gpsd and set it in alfred via unix socket. The alfred server must

main.c
@@ -49,6 +49,7 @@ static void alfred_usage(void)
 	printf("                              accepts data from slaves and synces it with\n");
 	printf("                              other masters\n");
 	printf("\n");
+	printf("  -u, --unix-path [path]      path to unix socket used for client-server communication (default: \""ALFRED_SOCK_PATH_DEFAULT"\")\n");
 	printf("  -v, --version               print the version\n");
 	printf("  -h, --help                  this help\n");
 	printf("\n");
@@ -66,6 +67,7 @@ static struct globals *alfred_init(int argc, char *argv[])
 		{"help",	no_argument,		NULL,	'h'},
 		{"req-version", required_argument,	NULL,	'V'},
 		{"modeswitch",  required_argument,	NULL,	'M'},
+		{"unix-path", 	required_argument,	NULL,	'u'},
 		{"version",	no_argument,		NULL,	'v'},
 		{NULL,		0,			NULL,	0},
 	};
@@ -79,10 +81,11 @@ static struct globals *alfred_init(int argc, char *argv[])
 	globals->best_server = NULL;
 	globals->clientmode_version = 0;
 	globals->mesh_iface = "bat0";
+	globals->unix_path = ALFRED_SOCK_PATH_DEFAULT;
 
 	time_random_seed();
 
-	while ((opt = getopt_long(argc, argv, "ms:r:hi:b:vV:M:", long_options,
+	while ((opt = getopt_long(argc, argv, "ms:r:hi:b:vV:M:u:", long_options,
 				  &opt_ind)) != -1) {
 		switch (opt) {
 		case 'r':
@@ -131,6 +134,9 @@ static struct globals *alfred_init(int argc, char *argv[])
 				return NULL;
 			}
 			globals->clientmode = CLIENT_MODESWITCH;
+			break;
+		case 'u':
+			globals->unix_path = optarg;
 			break;
 		case 'v':
 			printf("%s %s\n", argv[0], SOURCE_VERSION);

man/alfred.8
@@ -50,6 +50,9 @@ Print the version
 .TP
 \fB\-h\fP, \fB\-\-help\fP
 Display a brief help message.
+.TP
+\fB\-u\fP, \fB\-\-unix-path\fP \fIpath\fP
+path to unix socket used for client-server communication.
 .
 .SH CLIENT OPTIONS
 .TP

server.c
@@ -285,7 +285,7 @@ int alfred_server(struct globals *globals)
 	if (create_hashes(globals))
 		return -1;
 
-	if (unix_sock_open_daemon(globals, ALFRED_SOCK_PATH))
+	if (unix_sock_open_daemon(globals))
 		return -1;
 
 	if (!globals->interface) {

commit 24fcc74783bd84908ffbdbee3599bacb78e6d7f8

alfred: gpsd: reject too long unix socket paths
The unix socket path needs to be stored in sockaddr_un::sun_path. But
strncpy() is silently truncating the string when the target buffer is too
small.

Reject such paths with a clear error message.

Fixes: 6c6a6f735054 ("alfred: Make unix socket path configurable")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

gpsd/alfred-gpsd.c
@@ -16,6 +16,12 @@ static int alfred_open_sock(struct globals *globals)
 {
 	struct sockaddr_un addr;
 
+	if (strlen(globals->unix_path) >= sizeof(addr.sun_path)) {
+		fprintf(stderr, "unix socket path too long\n");
+		globals->unix_sock = -1;
+		return -1;
+	}
+
 	globals->unix_sock = socket(AF_LOCAL, SOCK_STREAM, 0);
 	if (globals->unix_sock < 0) {
 		perror("can't create unix socket");