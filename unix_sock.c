open-mesh-mirror/alfred

commit 94b3c4b5744f79743d9bcd7148c86b2776d1ce75

alfred: receive data with valid source on unix sock without active interface
Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>
Signed-off-by: Sven Eckelmann <sven@narfation.org>

unix_sock.c
@@ -108,10 +108,6 @@ static int unix_sock_add_data(struct globals *globals,
 	int len, data_len, ret = -1;
 	struct interface *interface;
 
-	interface = netsock_first_interface(globals);
-	if (!interface)
-		goto err;
-
 	len = ntohs(push->header.length);
 
 	if (len < (int)(sizeof(*push) - sizeof(push->header)))
@@ -133,11 +129,26 @@ static int unix_sock_add_data(struct globals *globals,
 	 * source addresses. Otherwise the data would not be
 	 * synced between primary servers.
 	 */
-	if (is_valid_ether_addr(data->source)) {
+	if (is_iface_disabled(globals->net_iface)) {
+		if (!is_valid_ether_addr(data->source))
+			goto err;
+
+		if (globals->opmode != OPMODE_PRIMARY)
+			goto err;
+
+	} else if (is_valid_ether_addr(data->source)) {
+		interface = netsock_first_interface(globals);
+		if (!interface)
+			goto err;
+
 		if (memcmp(data->source, &interface->hwaddr, ETH_ALEN) != 0 &&
 		    globals->opmode != OPMODE_PRIMARY)
 			goto err;
 	} else {
+		interface = netsock_first_interface(globals);
+		if (!interface)
+			goto err;
+
 		memcpy(data->source, &interface->hwaddr, ETH_ALEN);
 	}
 

commit b268623c2af794df4a542c7a023902956e3302ce

alfred: update stored interface list on interface change
The interface list received via ALFRED_CHANGE_INTERFACE was passed to
netsock_set_interfaces() but globals->net_iface kept the value from the
daemon start. But the stale string is used by unix_sock_add_data() to
decide whether interface operations are disabled ('none'). And for the none
interface, it is required to be in primary mode and to prefill the
data->source address.

Store a copy of the new interface list in globals->net_iface to dynamically
switch between the disabled/not disabled mode.

Fixes: 94b3c4b5744f ("alfred: receive data with valid source on unix sock without active interface")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

unix_sock.c
@@ -396,6 +396,7 @@ unix_sock_change_iface(struct globals *globals,
 		       struct alfred_change_interface_v0 *change_iface,
 		       int client_sock)
 {
+	char *net_iface;
 	int ret = -1;
 	int len;
 
@@ -413,6 +414,13 @@ unix_sock_change_iface(struct globals *globals,
 			goto err;
 		}
 	}
+
+	net_iface = strdup(change_iface->ifaces);
+	if (!net_iface)
+		goto err;
+
+	free(globals->net_iface);
+	globals->net_iface = net_iface;
 
 	netsock_set_interfaces(globals, change_iface->ifaces);
 